2f8d215409f2802098ad04b31a7b52b1cf000ef6
jason13official <jason13official@gmail.com>
2024-09-14 21:29:31 -0400

    fix: heal AbstractSummon via isFood check

--- neoforge/src/main/java/com/cursee/summons/core/common/entity/AbstractSummon.java
+++ neoforge/src/main/java/com/cursee/summons/core/common/entity/AbstractSummon.java
@@ -1,8 +1,11 @@
 package com.cursee.summons.core.common.entity;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public abstract class AbstractSummon extends TamableAnimal {
@@ -13,5 +16,19 @@ public abstract class AbstractSummon extends TamableAnimal {
 
     public int secondsToTicks(float seconds) {
         return (int)(seconds * 20f);
+    }
+
+    @Override
+    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+
+        if (this.getHealth() < this.getMaxHealth() && isFood(player.getItemInHand(hand))) {
+
+            this.heal(2f);
+            player.getItemInHand(hand).shrink(1);
+
+            return InteractionResult.CONSUME;
+        }
+
+        return super.mobInteract(player, hand);
     }
 }
